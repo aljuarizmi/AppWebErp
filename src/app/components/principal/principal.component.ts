cc119fd917491cd41bbe9d3bc14b27a58603746e
aljuarizmi <aljuarizmi@gmail.com>
2025-03-20 09:43:01 -0500

    Se agrega a la pagina principal la funcionalidad de ocultar/mostrar el menu

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -3,19 +3,23 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { MenuComponent } from '../menu/menu.component';
 import { AuthService } from '../../services/auth.service';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-principal',
-  imports: [CommonModule,RouterModule,MenuComponent],
+  imports: [CommonModule,RouterModule,MenuComponent,MatIcon],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
+  isMenuHidden = false; // Estado del menú
   constructor(private authService: AuthService, private router: Router) {};
   logout() {
     if(confirm("¿Seguro que desea cerrar sesion?")){
       this.authService.logout();
     }
-    //this.router.navigate(['/login']);
+  }
+  toggleMenu() {
+    this.isMenuHidden = !this.isMenuHidden;
   }
 }
