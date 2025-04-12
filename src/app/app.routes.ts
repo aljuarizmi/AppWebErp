245c47b1f0007dff4fdbbff7008810b43edc8551
aljuarizmi <aljuarizmi@gmail.com>
2025-04-12 16:14:00 -0500

    Se agrega el componente M03S03N01 para el registro de tipos de cambio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,12 @@ import { ErrorComponent } from './components/error/error.component';
 import { M00S01N02Component } from './components/modules/SY/M00S01N02.component';
 import { M00S01N80Component } from './components/modules/SY/M00S01N80.component';
 import { M00S01N05Component } from './components/modules/SY/M00S01N05.component';
+import { M03S03N01Component } from './components/modules/CM/M03S03N01.component';
 
 export const routes: Routes = [
     /*{ path: '', redirectTo: 'login', pathMatch: 'full' },*/
     {path:'',component:LoginComponent},
-    { path: 'login', component: LoginComponent },
+    {path: 'login', component: LoginComponent },
     {   path: 'principal', 
         component: PrincipalComponent,
         canActivate:[AuthGuard],
@@ -25,9 +26,13 @@ export const routes: Routes = [
             {   path:'M00S01N80',
                 component:M00S01N80Component,
                 canActivate:[AuthGuard]},
-                {   path:'M00S01N05',
-                    component:M00S01N05Component,
-                    canActivate:[AuthGuard]}
+            {   path:'M00S01N05',
+                component:M00S01N05Component,
+                canActivate:[AuthGuard]},
+            {   path:'M03S03N01',
+                component:M03S03N01Component,
+                canActivate:[AuthGuard]
+            }
     ]
     },
     //{ path: '**', redirectTo: 'login' }
---
+++ src/app/components/modules/CM/M03S03N01.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-m03-s03-n01',
+  imports: [],
+  templateUrl: './m03s03n01.component.html',
+  styleUrl: './m03s03n01.component.css'
+})
+export class M03S03N01Component {
+
+}
